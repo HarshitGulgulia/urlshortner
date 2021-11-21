5b47a41ca24d716edb50bb3789aeb982dff94c99
ashish-gopalika <meashishgopalika@gmail.com>
2021-11-21 06:18:16 +0530

    navbar added

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kushagra/Documents/git_repo/tally2021/urlshortner/virenv/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
